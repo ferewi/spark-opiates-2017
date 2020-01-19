6435703cec362c6261c06a84b86ce6ac4e7ddf4b
Ferdinand Rewicki <ferdinand.rewicki@uni-jena.de>
2020-01-19 18:30:50 +0100

    add information on drug deaths and geo locations

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ An Apache Spark project about opiate prescriptions in the USA in 2017
 
 ## Data Sources
 * Income Tax 2017 [https://www.irs.gov/statistics/soi-tax-stats-individual-income-tax-statistics-2017-zip-code-data-soi](https://www.irs.gov/statistics/soi-tax-stats-individual-income-tax-statistics-2017-zip-code-data-soi)
-  * Spalte A00100 (AGI) Beträge in Tausend Dollar
 
 * Opioids 2017: [https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/OpioidMap_Medicare_PartD](https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/OpioidMap_Medicare_PartD)  
 
@@ -14,3 +13,9 @@ An Apache Spark project about opiate prescriptions in the USA in 2017
 * US Census Data 2010: [https://www.kaggle.com/census/us-population-by-zip-code#population_by_zip_2010.csv](https://www.kaggle.com/census/us-population-by-zip-code#population_by_zip_2010.csv)
 
 * US ZIP/FIPS crossreference: [https://wonder.cdc.gov/wonder/sci_data/codes/fips/type_txt/cntyxref.asp](https://wonder.cdc.gov/wonder/sci_data/codes/fips/type_txt/cntyxref.asp)
+
+* Drug Deaths 2018: [https://www.cdc.gov/nchs/health_policy/Provisional-Drug-Overdose-Deaths-Counts-and-Rates-by-County-2018.pdf](https://www.cdc.gov/nchs/health_policy/Provisional-Drug-Overdose-Deaths-Counts-and-Rates-by-County-2018.pdf)
+
+* State Longitude/Latitude: [https://developers.google.com/public-data/docs/canonical/states_csv](https://developers.google.com/public-data/docs/canonical/states_csv)
+
+* County Longitude/Latitude: [https://en.wikipedia.org/wiki/User:Michael_J/County_table](https://en.wikipedia.org/wiki/User:Michael_J/County_table)
--- spark.py
+++ spark.py
@@ -17,7 +17,10 @@ opioids = spark.read.format("csv").option("header", "true").load(basepath + "opi
 npidata = spark.read.format("csv").option("header", "true").load(basepath + "npidata_20050523-20171210.csv")
 taxdata = spark.read.format("csv").option("header", "true").load(basepath + "17zpallagi.csv")
 popdata = spark.read.format("csv").option("header", "true").load(basepath + "population_by_zip_2010.csv")
+deaths = spark.read.format("csv").option("header", "true").load(basepath + "deaths_by_county_2018.csv")
 statefips = spark.read.format("csv").option("header", "true").load(basepath + "state_fips.csv")
+stateLongLat = spark.read.format("csv").option("header", "true").load(basepath + "states_long_lat.csv")
+fipsLongLat = spark.read.format("csv").option("header", "true").load(basepath + "fips_long_lat.csv")
 fipsdataRDD = sc.textFile(basepath + "zipfips/*")
 
 presc.registerTempTable("presc")
@@ -26,6 +29,9 @@ npidata.registerTempTable("npidata")
 taxdata.registerTempTable("taxdata")
 popdata.registerTempTable("popdata")
 statefips.registerTempTable("statefips")
+deaths.registerTempTable("deaths")
+stateLongLat.registerTempTable("stateLongLat")
+fipsLongLat.registerTempTable("fipsLongLat")
 
 # Vorverarbeitung fuer Zip to FIPS mapping
 fipsdataRDD = fipsdataRDD.map(lambda str: [str[:5], str[23:25], str[25:28], str[28:]])
@@ -35,6 +41,7 @@ fipsdata = spark.sql("""
     SELECT
         f.zipcode AS zipcode,
         FIRST(f.state_abbr) AS state,
+        FIRST(sf.state_name) AS state_name,
         FIRST(sf.fips) AS state_fips,
         FIRST(f.county_fips) AS county_fips,
         FIRST(CONCAT(sf.fips, f.county_fips)) as fips,
@@ -45,15 +52,6 @@ fipsdata = spark.sql("""
     ORDER BY f.zipcode
 """)
 fipsdata.registerTempTable("fipsdata")
-
-#popdata = spark.sql("""
-#    SELECT
-#        Id2 as zipcode,
-#        `Total; Estimate; Total population` as population,
-#        "FOO" as geo_id
-#    FROM popdata
-#""")
-#popdata.registerTempTable("popdata")
 
 # Reduziere prescribtion data auf Zeilen mit Opiaten
 genericNames = spark.sql("SELECT DISTINCT `Generic Name` FROM opioids")
@@ -86,6 +84,9 @@ presc.registerTempTable("presc")
 popdata = spark.sql("""
     SELECT
         f.fips AS fips,
+        FIRST(f.state) AS state,
+        FIRST(f.state_name) AS state_name,
+        FIRST(f.county) AS county,
         SUM(p.population) as population
     FROM popdata AS p
     INNER JOIN fipsdata f ON p.zipcode = f.zipcode
@@ -93,7 +94,7 @@ popdata = spark.sql("""
 """)
 popdata.registerTempTable("popdata")
 
-# aggrgate Prescriptions by zipcode
+# aggrgate Prescriptions by fipscode
 presc = spark.sql("""
     SELECT
         f.fips AS fips,
@@ -117,9 +118,14 @@ taxdata = spark.sql("""
 taxdata.registerTempTable("taxdata")
 
 # assemble dataset
-merged = spark.sql("""
+byFips = spark.sql("""
     SELECT
         pp.fips,
+        FIRST(pp.state) as state,
+        FIRST(pp.state_name) as state_name,
+        TRIM(LOWER(FIRST(pp.county))) AS county,
+        FIRST(fll.latitude) as latitude,
+        FIRST(fll.longitude) as longitude,
         SUM(pp.population)/IF(COUNT(t.agi_stub) > 0, COUNT(t.agi_stub), 1) as population,
         SUM(t.N1) as num_tax_returns,
         SUM(t.A00100) as total_agi,
@@ -131,19 +137,52 @@ merged = spark.sql("""
             WHEN SUM(t.A00100)/SUM(t.N1) < 100 THEN 4
             WHEN SUM(t.A00100)/SUM(t.N1) < 200 THEN 5
             WHEN SUM(t.A00100)/SUM(t.N1) >= 200 THEN 6
-            ELSE 0
+        ELSE 0
         END as agi_group,
-        COALESCE(SUM(p.claim_count)/IF(COUNT(t.agi_stub) > 0, COUNT(t.agi_stub), 1), -1) as claim_count,
-        COALESCE(SUM(p.claim_count)/SUM(pp.population),-1) as claim_rate
+        COALESCE(SUM(p.claim_count)/IF(COUNT(t.agi_stub) > 0, COUNT(t.agi_stub), 1), 0) as claim_count,
+        COALESCE(SUM(p.claim_count)/SUM(pp.population), 0) as claim_rate,
+        COALESCE(SUM(IF(d.deaths_by_residence = '^', 0, d.deaths_by_residence))/IF(COUNT(t.agi_stub) > 0, COUNT(t.agi_stub), 1), 0) as deaths_by_residence,
+        COALESCE(SUM(IF(d.deaths_by_residence = '^', 0, d.deaths_by_residence))/SUM(pp.population), 0) as death_rate
     FROM popdata as pp
+    LEFT JOIN deaths as d ON TRIM(LOWER(pp.county)) = TRIM(LOWER(d.county)) AND TRIM(LOWER(pp.state_name)) = TRIM(LOWER(d.state))
     LEFT JOIN taxdata as t ON pp.fips = t.fips
     LEFT JOIN presc as p ON pp.fips = p.fips
+    LEFT JOIN fipsLongLat fll ON pp.fips = fll.fips
     GROUP BY pp.fips
+    ORDER BY deaths_by_residence DESC
 """)
-merged.registerTempTable("merged")
-merged.toPandas().to_csv("results_by_fips.csv", encoding="UTF-8")
+byFips.registerTempTable("byFips")
+byFips.toPandas().to_csv("results_by_fips.csv", encoding="UTF-8")
 
-
+byState = spark.sql("""
+    SELECT
+        bf.state as state,
+        FIRST(bf.state_name) as state_name,
+        FIRST(sll.latitude) as latitude,
+        FIRST(sll.longitude) as longitude,
+        SUM(bf.population) as population,
+        SUM(bf.num_tax_returns) as num_tax_returns,
+        SUM(bf.total_agi) as total_agi,
+        SUM(bf.total_agi)/SUM(bf.num_tax_returns) as mean_agi,
+        CASE
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) < 25 THEN 1
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) < 50 THEN 2
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) < 75 THEN 3
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) < 100 THEN 4
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) < 200 THEN 5
+            WHEN SUM(bf.total_agi)/SUM(bf.num_tax_returns) >= 200 THEN 6
+        ELSE 0
+        END as agi_group,
+        SUM(bf.claim_count) as claim_count,
+        SUM(bf.claim_count)/SUM(bf.population) as claim_rate,
+        SUM(bf.deaths_by_residence) as deaths_by_residence,
+        SUM(bf.deaths_by_residence)/SUM(bf.population) as death_rate
+    FROM byFips AS bf
+    LEFT JOIN stateLongLat AS sll ON bf.state = sll.state
+    GROUP BY bf.state
+    ORDER BY deaths_by_residence DESC
+""")
+byState.toPandas().to_csv("results_by_state.csv", encoding="UTF-8")
 # zip level
 #spark.sql("SELECT STATE as state, SUBSTR(zipcode, 0, 3) as zip, SUM(N1) as tax_returns FROM taxdata GROUP BY state, SUBSTR(zipcode, 0, 3) ")
 
